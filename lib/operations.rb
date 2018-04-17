59a11c1f657d31450ea99f8eea72fdce3fc779ed
mariesonko62@gmail.com <mariesonko62@gmail.com>
2018-04-17 15:39:46 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -4,8 +4,6 @@ def unsafe? (speed)
 else
 	puts false
 end
+end 
+#def not_safe?(speed) speed< 40 || speed > 60 ? true : false end
 
-def not_safe?(speed)
-	speed< 40 || speed > 60 ? true : false
-end
-end
